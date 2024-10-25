e934127c92c4b14a8e3b8b5814789599f952ba5c
JakubArtysiewicz <jakub.artysiewicz@infotech.edu.pl>
2024-10-25 10:50:53 +0200

    Dodanie drugiej funkcji z trapezami

--- calka.py
+++ calka.py
@@ -9,4 +9,14 @@ def liczenie_calki(a,b,przyblizenie):
         wynik += y*x
     return wynik
 
+def liczenie_calki_trapezy(a,b,przyblizenie):
+    wynik = 0
+    x = (b-a)/przyblizenie
+    for i in range(przyblizenie):
+        y = funkcja(a+i*x)
+        y2 = funkcja(a+(i+1)*x)
+        wynik += ((y+y2)/2)*x
+    return wynik
+
 print(liczenie_calki(0,4,10000000))
+print(liczenie_calki_trapezy(0,4,1000))
